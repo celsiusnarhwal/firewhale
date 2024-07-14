64986b8d7d267c8e4ff7e1d7bd253756ebfd208d
celsius narhwal <hello@celsiusnarhwal.dev>
2024-07-13 22:50:53 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 caddy start
 
 while true; do
-  curl localhost:2019/load -H "Content-Type:application/json" -d $(firewhale generate --json)
+  curl -s localhost:2019/load -H "Content-Type:application/json" -d $(firewhale generate --json)
   sleep ${FIREWHALE_REFRESH_INTERVAL:-30}
 done
