b37f71e001cd335534960572ee3e277f5ec18b47
celsius narhwal <hello@celsiusnarhwal.dev>
2024-07-15 23:48:05 -0400

    Use `pydantic-settings` for settings

--- firewhale/main.py
+++ firewhale/main.py
@@ -1,64 +1,53 @@
 import os
 import subprocess
 import sys
+import time
 import typing as t
+from contextlib import redirect_stdout
 from datetime import datetime
 from pathlib import Path
-from tempfile import TemporaryDirectory
 
-import durationpy
+import httpx
 import typer
 from docker import DockerClient
-from jinja2 import Template
+from jinja2 import Environment, FileSystemLoader
 from loguru import logger
-from pydantic import Field, TypeAdapter, ValidationError
+
+from firewhale.settings import FirewhaleSettings
+
+settings = FirewhaleSettings()
+
+jinja = Environment(
+    loader=FileSystemLoader(Path(__file__).parent / "templates"),
+    trim_blocks=True,
+    lstrip_blocks=True,
+)
+
+app = typer.Typer(
+    no_args_is_help=True, add_completion=False, pretty_exceptions_show_locals=False
+)
 
 
-class Matcher(t.TypedDict):
-    name: str
-    rules: list
+@app.command("generate")
+def generate():
+    """
+    Generate a Caddy configuration for Firewhale.
+    """
 
+    class Matcher(t.TypedDict):
+        name: str
+        rules: list
 
-HERE = Path(__file__).parent
-
-app = typer.Typer(no_args_is_help=True, add_completion=False)
-
-
-@app.command("generate", help="Generate a Caddy configuration for Firewhale.")
-def generate(
-        json: bool = typer.Option(
-            False, "--json", help="Generate a JSON configuration instead of a Caddyfile."
-        )
-):
-    dc = DockerClient("unix://var/run/docker.sock")
-
-    label_prefix = os.getenv("FIREWHALE_LABEL_PREFIX", "firewhale")
-
-    # Validate FIREWHALE_PORT and FIREWHALE_HTTP_STATUS_CODE values
-    port_ta = TypeAdapter(t.Annotated[int, Field(ge=0, le=65535)])
-    status_code_ta = TypeAdapter(t.Annotated[int, Field(ge=100, le=599)])
-
-    try:
-        port = port_ta.validate_python(os.getenv("FIREWHALE_PORT", 2375))
-    except ValidationError:
-        raise ValueError(
-            "FIREWHALE_PORT must be an integer between and 65535."
-        ) from None
-
-    try:
-        fallback_status_code = status_code_ta.validate_python(
-            os.getenv("FIREWHALE_HTTP_STATUS_CODE", 403)
-        )
-    except ValidationError:
-        raise ValueError(
-            "FIREWHALE_HTTP_STATUS_CODE must be an integer between 100 and 599."
-        ) from None
+    if settings.dev_mode:
+        dc = DockerClient(**settings.dev_docker_opts)
+    else:
+        dc = DockerClient("unix:///var/run/docker.sock")
 
     matchers = []
 
     for container in dc.containers.list():
-        read_label = container.labels.get(f"{label_prefix}.read")
-        write_label = container.labels.get(f"{label_prefix}.write")
+        read_label = container.labels.get(f"{settings.label_prefix}.read")
+        write_label = container.labels.get(f"{settings.label_prefix}.write")
 
         # Write a request matcher for read access to /events, /_ping, and /version
         if read_label or write_label:
@@ -99,40 +88,43 @@ def generate(
 
                 matchers.append(Matcher(name=f"{container.name}_write", rules=rules))
 
-    with TemporaryDirectory() as tmpdir:
-        template = Template(
-            (Path(__file__).parent / "Caddyfile.template.txt").read_text()
-        )
-        caddyfile = Path(tmpdir) / "Caddyfile"
-        caddyfile.write_text(
-            template.render(
-                matchers=matchers, port=port, fallback_status_code=fallback_status_code
+    template = jinja.get_template("Caddyfile.template.txt")
+
+    caddyfile = template.render(
+        matchers=matchers,
+        port=settings.port,
+        status_code=settings.http_status_code,
+    )
+
+    print(caddyfile)
+
+    return caddyfile
+
+
+@app.command("start", hidden=True, add_help_option=False)
+def _start():
+    """
+    Start Firewhale.
+    """
+    logger.info(f"Listening on port {settings.port}")
+    logger.info(f"Reloading configuration every {settings.reload_interval_seconds} seconds")
+
+    subprocess.run(["caddy", "start"])
+
+    while True:
+        with redirect_stdout(open(os.devnull, "w")):
+            httpx.post(
+                "http://localhost:2019/load",
+                headers={"Content-Type": "text/caddyfile"},
+                content=generate(),
             )
-        )
-        subprocess.run(["caddy", "fmt", "--overwrite", str(caddyfile)])
 
-        if json:
-            print(
-                subprocess.check_output(
-                    ["caddy", "adapt", "--config", str(caddyfile)]
-                ).decode("utf-8")
-            )
-        else:
-            print(caddyfile.read_text())
+        time.sleep(settings.reload_interval_seconds)
 
 
-@app.command("duration", hidden=True)
-def duration(d: str):
-    try:
-        print(durationpy.from_str(d).total_seconds())
-    except ValueError:
-        raise ValueError(
-            "FIREWHALE_REFRESH_INTERVAL must be a valid Go-style duration string. "
-            "https://pkg.go.dev/time#ParseDuration"
-        ) from None
-
-
-@app.callback(epilog=f"© {datetime.now().astimezone().year} celsius narhwal. Thank you kindly for your attention.")
+@app.callback(
+    epilog=f"© {datetime.now().astimezone().year} celsius narhwal. Thank you kindly for your attention."
+)
 def main():
     """
     Firewhale is a proxy for the Docker socket.
@@ -142,10 +134,10 @@ def main():
 logger.remove()
 logger.add(
     level="INFO",
-    format="{time:YYYY/MM/DD HH:mm:ss.SS} {level}  {message}",
+    format="{time:YYYY/MM/DD HH:mm:ss.SSS} {level}  {message}",
     sink=sys.stderr,
 )
-logger.level("WARNING", color="<yellow>")
+logger.level("INFO", color="blue")
 
 if __name__ == "__main__":
     app()
---
+++ firewhale/settings.py
@@ -0,0 +1,35 @@
+import typing as t
+
+import durationpy
+from pydantic import Field, field_validator
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class FirewhaleSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="FIREWHALE_")
+
+    port: int = Field(2375, ge=0, le=65535)
+    http_status_code: int = Field(403, ge=100, le=699)
+    reload_interval: str = "30s"
+    label_prefix: str = "firewhale"
+
+    dev_mode: bool = False
+    dev_docker_opts: t.Optional[dict] = None
+
+    # noinspection PyMethodParameters
+    @field_validator("reload_interval")
+    def validate_reload_interval(cls, v):
+        try:
+            durationpy.from_str(v)
+        except ValueError:
+            raise ValueError(
+                "FIREWHALE_RELOAD_INTERVAL must be in the format of a Go duration string. "
+                "https://pkg.go.dev/time#ParseDuration"
+            ) from None
+
+        return v
+
+    @property
+    def reload_interval_seconds(self):
+        seconds = durationpy.from_str(self.reload_interval).total_seconds()
+        return int(seconds) if seconds.is_integer() else seconds
